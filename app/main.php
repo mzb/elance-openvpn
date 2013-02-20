a05be40f15c6a4cb4e246ec10cef7c8c0d6d8ae1
Mateusz Buczek <matbuczek@gmail.com>
2013-02-20 03:02:17 +0100

    Add LayoutView and Bootstrap. Create initial layout and user list template.

--- app/main.php
+++ app/main.php
@@ -3,10 +3,25 @@
 require '../lib/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim();
+require '../lib/view.php';
 
-$app->get('/', function() {
-  phpinfo();
+
+$app = new \Slim\Slim(array(
+  'templates.path' => __DIR__ . '/templates',
+  'view' => new LayoutView()
+));
+$app->view()->setLayout('layout.phtml');
+
+$app->get('/', function() use ($app) {
+  $app->render('dashboard.phtml');
 });
+
+$app->get('/users', function() use ($app) {
+  $app->render('users/index.phtml');
+})->name('users');
+
+$app->get('/groups', function() use ($app) {
+
+})->name('groups');
 
 return $app;
---
+++ lib/view.php
@@ -0,0 +1,30 @@
+<?php
+
+class LayoutView extends \Slim\View
+{
+  protected $layout;
+
+  public function setLayout($layout)
+  {
+    $this->layout = $this->getTemplatesDirectory() . '/' . ltrim($layout, '/');
+    if (!file_exists($this->layout)) {
+      throw new RuntimeException(sprintf('Layout not found: "%s"', $this->layout));
+    }
+  }
+
+  public function render($template)
+  {
+    $this->setTemplate($template);
+
+    $this->data['app'] = \Slim\Slim::getInstance();
+    extract($this->data);
+
+    ob_start();
+    require $this->templatePath;
+    $__content__ = ob_get_clean();
+
+    ob_start();
+    require $this->layout;
+    return ob_get_clean();
+  }
+}
